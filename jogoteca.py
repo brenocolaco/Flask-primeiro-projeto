e0b64aa846a34f9cdaff944b07daa011527178d0
Breno Colaço <54214580+brenocolaco@users.noreply.github.com>
2024-11-18 20:39:36 -0300

    Adicionando conteudo dinamico

--- jogoteca.py
+++ jogoteca.py
@@ -1,10 +1,21 @@
 from flask import Flask, render_template
 
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
+
 app = Flask(__name__)
 
 @app.route('/home')
 def hello():
-    lista = ['Tetris', 'Skyrim', 'Crash']
+    jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+    jogo2 = Jogo('God of War', 'Rack n Slash', 'PS2')
+    jogo3 = Jogo('Mortal Kombat', 'Luta', 'SNES')
+
+    lista = [jogo1, jogo2, jogo3]
     return render_template('lista.html', titulo='Jogos', jogos = lista)
 
 app.run()
